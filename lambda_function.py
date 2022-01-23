a963ed4850670661e36d0ae326a12e737d4355a5
Sachintech-github <95368495+Sachintech-github@users.noreply.github.com>
2022-01-23 17:45:21 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,8 @@
-import boto3
-region = 'ap-south-1'
-instances = ['i-0167aaed7ed0bac9a']
-ec2 = boto3.client('ec2', region_name=region)
+import json
 
 def lambda_handler(event, context):
-    ec2.start_instances(InstanceIds=instances)
-    print('started your instances: ' + str(instances))
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!....... (SACHIN-EDITED')
+    }
